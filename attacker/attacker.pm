daa3209fa3a92fd30a470a1f2dc64d718acbfe01
Van Chan Ngo <chan.ngo2203@gmail.com>
2017-09-20 16:36:29 -0400

    update all

--- attacker/attacker.pm
+++ attacker/attacker.pm
@@ -11,11 +11,13 @@ const int w6 = 4;
 const int w7 = 2;
 const int w8 = 1;
 const int nsteps = 8;
+//const int secret = 0;
+//const secret = 255;
 
 // probabilities of checking that guess is smaller than secret
-const double p1 = 0.9994;
+const double p1 = 0.994;
 // probabilities of checking that guess is bigger than secret
-const double p2 = 0.9994;
+const double p2 = 0.994;
 
 module attacker
 
@@ -29,40 +31,40 @@ module attacker
 	success : bool;
 	
 	// step 1
-	[] secretMin = 0 & steps = 1 & secretMin + w1 <= secret -> p1:(secretMin'=secretMin+w1)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
-	[] secretMin = 0 & steps = 1 & secretMin + w1 > secret -> (1-p2):(secretMin'=secretMin+w1)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin = 0 & steps = 1 & secretMin + w1 <= secret -> p1:(secretMin'=secretMin+w1)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin = 0 & steps = 1 & secretMin + w1 > secret -> (1-p2):(secretMin'=secretMin+w1)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
 	
 	// step 2
-	[] secretMin <= w1 & steps = 2 & secretMin + w2 <= secret -> p1:(secretMin'=secretMin+w2)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
-	[] secretMin <= w1 & steps = 2 & secretMin + w2 > secret -> (1-p2):(secretMin'=secretMin+w2)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= w1 & steps = 2 & secretMin + w2 <= secret -> p1:(secretMin'=secretMin+w2)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= w1 & steps = 2 & secretMin + w2 > secret -> (1-p2):(secretMin'=secretMin+w2)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
 	
 	// step 3
-	[] secretMin <= (w1 + w2) & steps = 3 & secretMin + w3 <= secret -> p1:(secretMin'=secretMin+w3)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
-	[] secretMin <= (w1 + w2) & steps = 3 & secretMin + w3 > secret -> (1-p2):(secretMin'=secretMin+w3)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2) & steps = 3 & secretMin + w3 <= secret -> p1:(secretMin'=secretMin+w3)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2) & steps = 3 & secretMin + w3 > secret -> (1-p2):(secretMin'=secretMin+w3)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
 	
 	// step 4
-	[] secretMin <= (w1 + w2 + w3) & steps = 4 & secretMin + w4 <= secret -> p1:(secretMin'=secretMin+w4)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
-	[] secretMin <= (w1 + w2 + w3) & steps = 4 & secretMin + w4 > secret -> (1-p2):(secretMin'=secretMin+w4)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2 + w3) & steps = 4 & secretMin + w4 <= secret -> p1:(secretMin'=secretMin+w4)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2 + w3) & steps = 4 & secretMin + w4 > secret -> (1-p2):(secretMin'=secretMin+w4)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
 	
 	// step 5
-	[] secretMin <= (w1 + w2 + w3 + w4) & steps = 5 & secretMin + w5 <= secret -> p1:(secretMin'=secretMin+w5)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
-	[] secretMin <= (w1 + w2 + w3 + w4) & steps = 5 & secretMin + w5 > secret -> (1-p2):(secretMin'=secretMin+w5)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2 + w3 + w4) & steps = 5 & secretMin + w5 <= secret -> p1:(secretMin'=secretMin+w5)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2 + w3 + w4) & steps = 5 & secretMin + w5 > secret -> (1-p2):(secretMin'=secretMin+w5)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
 	
 	// step 6
-	[] secretMin <= (w1 + w2 + w3 + w4 + w5) & steps = 6 & secretMin + w6 <= secret -> p1:(secretMin'=secretMin+w6)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
-	[] secretMin <= (w1 + w2 + w3 + w4 + w5) & steps = 6 & secretMin + w6 > secret -> (1-p2):(secretMin'=secretMin+w6)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2 + w3 + w4 + w5) & steps = 6 & secretMin + w6 <= secret -> p1:(secretMin'=secretMin+w6)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2 + w3 + w4 + w5) & steps = 6 & secretMin + w6 > secret -> (1-p2):(secretMin'=secretMin+w6)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
 	
 	// step 7
-	[] secretMin <= (w1 + w2 + w3 + w4 + w5 + w6) & steps = 7 & secretMin + w7 <= secret -> p1:(secretMin'=secretMin+w7)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
-	[] secretMin <= (w1 + w2 + w3 + w4 + w5 + w6) & steps = 7 & secretMin + w7 > secret -> (1-p2):(secretMin'=secretMin+w7)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2 + w3 + w4 + w5 + w6) & steps = 7 & secretMin + w7 <= secret -> p1:(secretMin'=secretMin+w7)&(steps'=steps+1) + (1-p1):(secretMin'=secretMin)&(steps'=steps+1);
+	[] !done & !check & secretMin <= (w1 + w2 + w3 + w4 + w5 + w6) & steps = 7 & secretMin + w7 > secret -> (1-p2):(secretMin'=secretMin+w7)&(steps'=steps+1) + p2:(secretMin'=secretMin)&(steps'=steps+1);
 
 	// step 8
-	[] secretMin <= (w1 + w2 + w3 + w4 + w5 + w6 + w7) & steps = 8 & secretMin + w8 <= secret -> p1:(secretMin'=secretMin+w8)&(check'=true) + (1-p1):(secretMin'=secretMin)&(check'=true);
-	[] secretMin <= (w1 + w2 + w3 + w4 + w5 + w6 + w7) & steps = 8 & secretMin + w8 > secret -> (1-p2):(secretMin'=secretMin+w8)&(check'=true) + p2:(secretMin'=secretMin)&(check'=true);
+	[] !done & !check & secretMin <= (w1 + w2 + w3 + w4 + w5 + w6 + w7) & steps = 8 & secretMin + w8 <= secret -> p1:(secretMin'=secretMin+w8)&(check'=true) + (1-p1):(secretMin'=secretMin)&(check'=true);
+	[] !done & !check & secretMin <= (w1 + w2 + w3 + w4 + w5 + w6 + w7) & steps = 8 & secretMin + w8 > secret -> (1-p2):(secretMin'=secretMin+w8)&(check'=true) + p2:(secretMin'=secretMin)&(check'=true);
 
 	// check
-	[] check & secretMin = secret -> (success'=true)&(done'=true);
-	[] check & secretMin != secret -> (success'=false)&(done'=true);
+	[] !done & check & secretMin = secret -> (success'=true)&(done'=true);
+	[] !done & check & secretMin != secret -> (success'=false)&(done'=true);
 	[] done -> true;
 
 endmodule
@@ -71,4 +73,5 @@ endmodule
 init secretMin = 0 & steps = 1 & secret >= 0 & secret <= 255 & check = false & done = false & success = false endinit
  
 // labels
-label "finished" = done; 
+label "finished" = done;
+
